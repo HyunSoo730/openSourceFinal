a85c20a7718410b949de76cbd721724bb9bb6247
HyunSoo730 <whgustn730@gmail.com>
2022-05-23 15:04:28 +0900

    프로젝트 시작을 위한 커밋!

---
+++ app/src/main/java/com/example/opensourcefinal/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.opensourcefinal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    //전설의 시작.
+
+}
